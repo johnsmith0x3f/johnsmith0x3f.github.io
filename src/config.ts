8a80ecc349c44a97ff1027282517ff4875ad7c7a
John Smith <johnsmith0x3f@gmail.com>
2025-03-28 08:50:05 +0000

    chore: update info

--- src/config.ts
+++ src/config.ts
@@ -7,31 +7,31 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Home',
+  title: "John's Blog",
   subtitle: 'johnsmith0x3f',
-  lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
+  lang: 'en', // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
-    hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-    fixed: false,     // Hide the theme color picker for visitors
+    hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    fixed: false, // Hide the theme color picker for visitors
   },
 	banner: {
     enable: false,
-    src: 'assets/images/banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-    position: 'center',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+    src: 'assets/images/banner.png', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    position: 'center', // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
     credit: {
-      enable: false,         // Display the credit text of the banner image
-      text: '',              // Credit text to be displayed
-      url: ''                // (Optional) URL link to the original artwork or artist's page
+      enable: false, // Display the credit text of the banner image
+      text: '', // Credit text to be displayed
+      url: '' // (Optional) URL link to the original artwork or artist's page
     }
   },
   toc: {
     enable: true,           // Display the table of contents on the right side of the post
     depth: 2                // Maximum heading depth to show in the table, from 1 to 3
   },
-  favicon: [    // Leave this array empty to use the default favicon
+  favicon: [ // Leave this array empty to use the default favicon
     {
-      src: '/favicon/favicon.ico',    // Path of the favicon, relative to the /public directory
-      sizes: '128x128',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+      src: '/favicon/favicon.ico', // Path of the favicon, relative to the /public directory
+      sizes: '128x128', // (Optional) Size of the favicon, set only if you have favicons of different sizes
     }
   ]
 }
--- src/content/posts/example.md
+++ src/content/posts/example.md
@@ -98,19 +98,18 @@ that last line which continues item 3 above).
 
 Here's a link to [a website](http://foo.bar), to a [local
 doc](local-doc.html), and to a [section heading in the current
-doc](#an-h2-header). Here's a footnote [^1].
+doc](#an-h2-header). Here's a footnote[^1].
 
 [^1]: Footnote text goes here.
 
 Tables can look like this:
 
-size material color
 
----
-
-9 leather brown
-10 hemp canvas natural
-11 glass transparent
+| size | material | color |
+| --- | --- | --- |
+| 9 | leather | brown |
+| 10 | hemp canvas | natural |
+| 11 | glass | transparent |
 
 Table: Shoes, their sizes, and what they're made of
 
