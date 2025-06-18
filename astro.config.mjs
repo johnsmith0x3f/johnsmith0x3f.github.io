f72eaf6c6652437ca7822c93e04ff0ad66ce1e67
John Smith <johnsmith0x3f@gmail.com>
2025-06-18 10:19:20 +0000

    feat: update site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
   site: "https://johnsmith0x3f.github.io",
-	base: "/fuwari",
+	base: "/",
   trailingSlash: "ignore",
   integrations: [
     tailwind(
